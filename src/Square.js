da2ee4128077553b26f93f894a9bb9e97b18d79d
Emil van Velden <emilvanvelden@gmail.com>
2020-12-17 09:15:29 +0200

    first commit

---
+++ src/Square.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Square = ({onClick, value})=>{
+  return (
+          <div className="square" onClick={onClick}>
+            {value}
+          </div>
+        );
+}
+
+
+export default Square;
